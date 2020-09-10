c519861a5d2a47ba1ea541edbbec0bafd8a1ead7
shobhit <shobhitsingh2007@gmail.com>
2020-09-10 23:47:27 +0530

    adding cx_oracle soda examples

---
+++ cx_oracle_soda/insertSODADocument2.py
@@ -0,0 +1,35 @@
+import configparser
+import cx_Oracle
+import json
+import requests
+
+config = configparser.ConfigParser()
+config.read('C:/Users/shobhit/Documents/Python/Config/config.txt')
+
+hostname =  config.get("DB_CONN_DETAILS","hostname")
+port =  config.get("DB_CONN_DETAILS","port")
+servicename =  config.get("DB_CONN_DETAILS","servicename")
+username =  config.get("DB_CONN_CRED","username")
+password =  config.get("DB_CONN_CRED","password")
+encoding = config.get("DB_CONN_DETAILS","encoding")
+
+print('Starting session...')
+try:
+    dsn = cx_Oracle.makedsn(hostname, port, service_name=servicename)
+    conn = cx_Oracle.connect(username,password,dsn,encoding=encoding)
+    print('Connected:' + conn.version)
+ 
+    soda = conn.getSodaDatabase()
+    collection = soda.createCollection("first_collection")
+ 
+    # using file
+    with open('jsonndata.json') as f:
+        data = json.load(f)
+ 
+    collection.insertOne(data)
+    conn.commit()
+    conn.close()
+    #print('connection closed...')
+except cx_Oracle.Error as e:
+    print('Error:', e)
+        
---
+++ cx_oracle_soda/retrieveSODADocData.py
@@ -0,0 +1,37 @@
+import configparser
+import cx_Oracle
+import json
+import requests
+
+config = configparser.ConfigParser()
+config.read('C:/Users/shobhit/Documents/Python/Config/config.txt')
+
+hostname =  config.get("DB_CONN_DETAILS","hostname")
+port =  config.get("DB_CONN_DETAILS","port")
+servicename =  config.get("DB_CONN_DETAILS","servicename")
+username =  config.get("DB_CONN_CRED","username")
+password =  config.get("DB_CONN_CRED","password")
+encoding = config.get("DB_CONN_DETAILS","encoding")
+
+print('Starting session...')
+try:
+    dsn = cx_Oracle.makedsn(hostname, port, service_name=servicename)
+    conn = cx_Oracle.connect(username,password,dsn,encoding=encoding)
+    print('Connected:' + conn.version)
+ 
+    soda = conn.getSodaDatabase()
+    coll = soda.openCollection("first_collection")
+    doc = coll.find()
+    docs = doc.getDocuments()
+    res = docs[0].getContent()
+ 
+    for i in res:
+        print (i["name"])
+ 
+    # dumping complete data as json file
+    json.dump(res, open ('data.json', 'w'), indent=4)
+ 
+    conn.close()
+    print('connection closed...')
+except cx_Oracle.Error as e:
+    print('Error:', e)
